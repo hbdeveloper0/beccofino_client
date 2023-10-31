5bae6660216c1d706818e6d7e27a85e9aadf6fe2
hybreathe43 <hbdeveloper.43@gmail.com>
2023-10-31 19:11:13 +0500

    update

--- src/views/pages/reservation/index.jsx
+++ src/views/pages/reservation/index.jsx
@@ -5,13 +5,22 @@ import MediaSlider from '../Media/components/slider/MediaSlider'
 import Navbar from '../../components/navbar/Navbar'
 import Reservation from '../../../assets/images/reservation/reservation.jpg'
 import BookRreservation from './BookReservation'
+import { Container, Grid } from '@mui/material'
+import Form from './Form'
 
 const Rreservation = () => {
     return (
         <div>
         <Navbar/>
           <MediaSlider img={Reservation} name='Reservation' />
+          <Grid  sx={{background:"#f0edea"}}>
           <BookRreservation/>
+          </Grid>
+          <Grid  sx={{background:"#f0edea"}}>
+          <Form/>
+         </Grid>
+
+          
             <Footer/>
         </div>
     )
