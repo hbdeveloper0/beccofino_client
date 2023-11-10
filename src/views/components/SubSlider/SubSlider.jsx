33873766813874c4337f2108d21f0d84a2fb8065
hybreathe43 <hbdeveloper.43@gmail.com>
2023-11-10 15:11:39 +0500

    update

--- src/views/components/SubSlider/SubSlider.jsx
+++ src/views/components/SubSlider/SubSlider.jsx
@@ -8,7 +8,7 @@ const SubSlider = (props) => {
             <Grid item xs={12}>
                 <Box sx={{ backgroundImage: `url(${props.img})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '400px' }}>
                     <Box  data-aos="zoom-in" data-aos-duration="1500" sx={{ backgroundColor: 'rgba(0, 0, 0, 0.7)', height: '400px', display: 'flex', alignItems: 'center', justifyContent: 'center',paddingTop:'100px' }}>
-                        <Typography variant='h1' sx={{color: '#F0EDEA', fontFamily: 'Montserrat, sans-serif', fontSize: '66px'}}>{props.name}</Typography>
+                        <Typography variant='h1' sx={{color: '#F0EDEA', fontFamily: 'Montserrat, sans-serif', fontSize: {xs:'46px',sm:"66"}}}>{props.name}</Typography>
                     </Box>
                 </Box>
             </Grid>
